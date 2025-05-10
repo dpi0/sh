acd97437037fcf21396723c67ccd8bfc5f649318
divyansh <dpi0.dev@proton.me>
2025-05-10 15:49:40 +0530

    Add yazi plugins and fix keymaps

--- yazi/init.lua
+++ yazi/init.lua
@@ -27,3 +27,79 @@ Status:children_add(function()
     " ",
   })
 end, 500, Status.RIGHT)
+
+-- https://github.com/h-hg/yamb.yazi
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+
+local path_sep = package.config:sub(1, 1)
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+if ya.target_family() == "windows" then
+  table.insert(bookmarks, {
+    tag = "Scoop Local",
+
+    path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
+    key = "p",
+  })
+  table.insert(bookmarks, {
+    tag = "Scoop Global",
+    path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
+    key = "P",
+  })
+end
+
+table.insert(bookmarks, {
+  tag = "Home",
+  path = home_path .. path_sep,
+  key = "h",
+})
+
+table.insert(bookmarks, {
+  tag = "projects",
+  path = home_path .. path_sep .. "projects" .. path_sep,
+  key = "p",
+})
+
+table.insert(bookmarks, {
+  tag = "Screenshots",
+  path = home_path .. path_sep .. "Screenshots" .. path_sep,
+  key = "s",
+})
+
+table.insert(bookmarks, {
+  tag = "temp",
+  path = home_path .. path_sep .. "temp" .. path_sep,
+  key = "t",
+})
+
+table.insert(bookmarks, {
+  tag = "Downloads",
+  path = home_path .. path_sep .. "Downloads" .. path_sep,
+  key = "d",
+})
+
+table.insert(bookmarks, {
+  tag = "notes",
+  path = home_path .. path_sep .. "notes" .. path_sep,
+  key = "n",
+})
+
+table.insert(bookmarks, {
+  tag = "Backup",
+  path = home_path .. path_sep .. "Backup" .. path_sep,
+  key = "b",
+})
+
+require("yamb"):setup({
+  -- Optional, the path ending with path seperator represents folder.
+  bookmarks = bookmarks,
+  -- Optional, recieve notification everytime you jump.
+  jump_notify = true,
+  -- Optional, the cli of fzf.
+  cli = "fzf",
+  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  -- Optional, the path of bookmarks
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
+      or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+})
