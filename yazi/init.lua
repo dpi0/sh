b1acdd3d00cfd1411ef736841f7c32e807c3e0af
divyansh <dpi0.dev@proton.me>
2025-12-07 21:12:58 +0530

    Refactor yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,4 +1,5 @@
--- Needed for linemode in yazi.toml to show size and modified time next to file list
+-- Show the file size and modified time in the file list "size_and_mtime"
+-- https://yazi-rs.github.io/docs/configuration/yazi#mgr.linemode
 function Linemode:size_and_mtime()
 	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
@@ -28,134 +29,29 @@ Status:children_add(function()
 	})
 end, 500, Status.RIGHT)
 
--- https://github.com/h-hg/yamb.yazi
--- You can configure your bookmarks by lua language
+-- YAMB
+-- https://github.com/h-hg/yamb.yazi#usage
 local bookmarks = {}
-
 local path_sep = package.config:sub(1, 1)
 local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
-if ya.target_family() == "windows" then
-	table.insert(bookmarks, {
-		tag = "Scoop Local",
-
-		path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
-		key = "p",
-	})
-	table.insert(bookmarks, {
-		tag = "Scoop Global",
-		path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
-		key = "P",
-	})
-end
-
-table.insert(bookmarks, {
-	tag = "Home",
-	path = home_path .. path_sep,
-	key = "h",
-})
-
-table.insert(bookmarks, {
-	tag = "projects",
-	path = home_path .. path_sep .. "projects" .. path_sep,
-	key = "p",
-})
-
-table.insert(bookmarks, {
-	tag = "Screenshots",
-	path = home_path .. path_sep .. "Screenshots" .. path_sep,
-	key = "s",
-})
-
-table.insert(bookmarks, {
-	tag = "temp",
-	path = home_path .. path_sep .. "temp" .. path_sep,
-	key = "t",
-})
-
-table.insert(bookmarks, {
-	tag = "Downloads",
-	path = home_path .. path_sep .. "Downloads" .. path_sep,
-	key = "d",
-})
-
-table.insert(bookmarks, {
-	tag = "notes",
-	path = home_path .. path_sep .. "notes" .. path_sep,
-	key = "n",
-})
-
-table.insert(bookmarks, {
-	tag = "Backup",
-	path = home_path .. path_sep .. "Backup" .. path_sep,
-	key = "b",
-})
-
 require("yamb"):setup({
-	-- Optional, the path ending with path seperator represents folder.
 	bookmarks = bookmarks,
-	-- Optional, recieve notification everytime you jump.
 	jump_notify = true,
-	-- Optional, the cli of fzf.
 	cli = "fzf",
-	-- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
 	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	-- Optional, the path of bookmarks
 	path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
 		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
 })
 
--- Always show device selection
--- https://github.com/Deepak22903/kdeconnect-send.yazi?tab=readme-ov-file#configuration-optional
-require("kdeconnect-send"):setup({
-	auto_select_single = true,
-})
-
--- PASTED FROM:
+-- GVFS
 -- https://github.com/boydaihungst/gvfs.yazi#installation
 require("gvfs"):setup({
-	-- (Optional) Allowed keys to select device.
 	which_keys = "1234567890qwertyuiopasdfghjklzxcvbnm-=[]\\;',./!@#$%^&*()_+{}|:\"<>?",
-
-	-- (Optional) Table of blacklisted devices. These devices will be ignored in any actions
-	-- List of device properties to match, or a string to match the device name:
-	-- https://github.com/boydaihungst/gvfs.yazi/blob/master/main.lua#L144
 	blacklist_devices = { { name = "Wireless Device", scheme = "mtp" }, { scheme = "file" }, "Device Name" },
-
-	-- (Optional) Save file.
-	-- Default: ~/.config/yazi/gvfs.private
 	save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
-
-	-- (Optional) Save file for automount devices. Use with `automount-when-cd` action.
-	-- Default: ~/.config/yazi/gvfs_automounts.private
 	save_path_automounts = os.getenv("HOME") .. "/.config/yazi/gvfs_automounts.private",
-
-	-- (Optional) Input box position.
-	-- Default: { "top-center", y = 3, w = 60 },
-	-- Position, which is a table:
-	-- 	`1`: Origin position, available values: "top-left", "top-center", "top-right",
-	-- 	     "bottom-left", "bottom-center", "bottom-right", "center", and "hovered".
-	--         "hovered" is the position of hovered file/folder
-	-- 	`x`: X offset from the origin position.
-	-- 	`y`: Y offset from the origin position.
-	-- 	`w`: Width of the input.
-	-- 	`h`: Height of the input.
 	input_position = { "center", y = 0, w = 60 },
-
-	-- (Optional) Select where to save passwords.
-	-- Default: nil
-	-- Available options: "keyring", "pass", or nil
 	password_vault = "keyring",
-
-	-- (Optional) Only need if you set password_vault = "pass"
-	-- Read the guide at SECURE_SAVED_PASSWORD.md to get your key_grip
 	key_grip = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-
-	-- (Optional) Auto-save password after mount.
-	-- Default: false
 	save_password_autoconfirm = true,
-	-- (Optional) mountpoint of gvfs. Default: /run/user/USER_ID/gvfs
-	-- On some system it could be ~/.gvfs
-	-- You can't decide this path, it will be created automatically. Only changed if you know where gvfs mountpoint is.
-	-- Use command `ps aux | grep gvfs` to search for gvfs process and get the mountpoint path.
-	-- root_mountpoint = (os.getenv("XDG_RUNTIME_DIR") or ("/run/user/" .. ya.uid())) .. "/gvfs"
 })
